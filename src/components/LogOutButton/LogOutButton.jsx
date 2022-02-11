4e73127257c0fe4fba5358affe2cb83ea29320e5
Leah Grim <grimchelate@gmail.com>
2022-02-11 16:16:36 -0600

    birds are appending to the DOM

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -8,10 +8,9 @@ function LogOutButton(props) {
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
-      className={props.className}
       onClick={() => dispatch({ type: 'LOGOUT' })}
     >
-      Log Out
+    <h2>  Log Out </h2>
     </button>
   );
 }
--- src/components/SightedList/SightedList.jsx
+++ src/components/SightedList/SightedList.jsx
@@ -1,4 +1,5 @@
-import "./SightedList.css"
+import "./SightedList.css";
+import LifeListDescription from "./LifeListDescription.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import React, { useState, useEffect } from "react";
 
@@ -9,7 +10,9 @@ function SightedList (){
   //setup dispatch
   const dispatch = useDispatch();
 
-
+function showDescription (){
+    console.log('we are in show description')
+}
     useEffect(()=> {
         dispatch({
           type: 'FETCH_CLIENT_LIST'
@@ -21,7 +24,40 @@ function SightedList (){
 
     return (
         <>
-        
+        <div className= "LifeListHeader">
+        <h1> Birds You've Logged</h1>
+        </div>
+        <div className="lifeListContainer"> 
+        {list && 
+            <div className= "lifeListDiv">
+                {list.map((bird, index) => (
+                    <div className="birdImage" key= {index}>   
+                                    {/* IMAGE OF BIRD */}
+                                    <img 
+                                        
+                                        src= {bird.image_path}
+                                        width= {350}
+                                        height={300}
+                                        onClick= { showDescription }
+                                    />
+                                    {/* BIRD COMMON NAME */}
+                                    <div className="birdCommonNameTitle"> 
+                                    <h2> {bird.Common_name} </h2>
+                                    </div>        
+                                    {/* DESCRIPTION CONTENT FOR BIRD */}
+                                    <div className="containerForBirdDescription" > 
+                                        <h3> {bird.Order} </h3>
+                                        <h3> {bird.Family_name}</h3>
+                                        <h3> {bird.Scientific_Name} </h3>
+                                        <h3> {bird.date_spotted} </h3>
+                                        <h3> {bird.description}</h3>
+                                        <h3> {bird.location_spotted} </h3>
+                                    </div>
+                    </div>
+                ))}
+            </div>
+            }
+        </div>
         </>
     )
 }
